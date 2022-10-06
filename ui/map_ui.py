e475be0cfc2b040960411759790946b931767b54
Richard Sherriff <sherriff8@gmail.com>
2022-10-06 16:35:35 +0100

    Added early map

--- ui/map_ui.py
+++ ui/map_ui.py
@@ -7,12 +7,12 @@ class MapUI(UI):
     def __init__(self, section,  x, y, tiles):
         super().__init__(section, x, y)
 
-        bd = [4, 4, 6, 1]  # Button Dimensions
+        bd = [12, 24, 7, 1]  # Button Dimensions
         button_tiles = tiles[bd[0]:bd[0] + bd[2], bd[1]:bd[1] + bd[3]]
         left_button = Button(x=bd[0], y=bd[1], width=bd[2],height=bd[3], click_action=ChangePlayerLocationAction(self.section.engine, "client"), tiles=button_tiles)
         self.add_element(left_button)
 
-        bd = [4, 5, 10, 1]  # Button Dimensions
+        bd = [28, 10, 11, 1]  # Button Dimensions
         button_tiles = tiles[bd[0]:bd[0] + bd[2], bd[1]:bd[1] + bd[3]]
         left_button = Button(x=bd[0], y=bd[1], width=bd[2],height=bd[3], click_action=ChangePlayerLocationAction(self.section.engine, "bloomsbury"), tiles=button_tiles)
         self.add_element(left_button)
